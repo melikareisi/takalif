627f6a94811323703df9eee825aa616fe573016d
melika reisi <126372164+melikareisi@users.noreply.github.com>
2023-05-06 09:16:55 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,60 +1,48 @@
 ﻿using System;
 
-class Program
+namespace App
 {
-    static void Main(string[] args)
+    class Project
     {
-        int[,] array = new int[,]
+        public const int SIZE = 3;
+        static void Main()
         {
-            {1, 4, 2},
-            {3, 6, 5},
-            {7, 9, 8}
-        };
-
-        int[] flattenedArray = FlattenArray(array); // تبدیل آرایه دوبعدی به آرایه یک بعدی
-        Array.Sort(flattenedArray); // مرتب کردن آرایه به صورت صعودی
-
-        int median = FindMedian(flattenedArray); // پیدا کردن میانه
-
-        Console.WriteLine("The median of the array is: " + median);
-        Console.ReadLine();
-    }
-
-    // تبدیل آرایه دوبعدی به آرایه یک بعدی
-    static int[] FlattenArray(int[,] array)
-    {
-        int rows = array.GetLength(0);
-        int columns = array.GetLength(1);
-        int[] flattenedArray = new int[rows * columns];
-        int index = 0;
-
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < columns; j++)
+            int[,] arr = new int[SIZE, SIZE];
+            int dupCount = 0;
+            int dupNum = 0;
+            for (int i = 0; i < SIZE; i++)
             {
-                flattenedArray[index] = array[i, j];
-                index++;
+                for (int j = 0; j < SIZE; j++)
+                {
+                    Console.WriteLine("Enter ["
+                        + i
+                        + "]["
+                        + j
+                        + "]: "
+                    );
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
             }
+
+            Console.WriteLine("Enter duplicate number : ");
+            dupNum = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < SIZE; i++)
+            {
+                for (int j = 0; j < SIZE; j++)
+                {
+                    if (arr[j, i] == dupNum)
+                        ++dupCount;
+                }
+            }
+
+            Console.WriteLine("Duplicate count for "
+                + dupNum
+                + "is : "
+                + dupCount
+            );
         }
 
-        return flattenedArray;
-    }
-
-    // پیدا کردن میانه
-    static int FindMedian(int[] array)
-    {
-        int length = array.Length;
-        int middleIndex = length / 2;
-
-        if (length % 2 == 0) // اگر تعداد اعداد زوج بود
-        {
-            int median = (array[middleIndex - 1] + array[middleIndex]) / 2;
-            return median;
-        }
-        else // اگر تعداد اعداد فرد بود
-        {
-            int median = array[middleIndex];
-            return median;
-        }
     }
 }
+//Duplicate
